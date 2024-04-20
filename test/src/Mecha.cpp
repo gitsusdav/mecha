06319a27e8f064d2cce7a6a5e270cdc50878525a
Elias Montilla <eliasbest634@gmail.com>
2024-04-20 21:09:13 +0200

    update test for to do

--- test/src/Mecha.cpp
+++ test/src/Mecha.cpp
@@ -46,4 +46,94 @@ int main()
        // assert_equal(actual, esperado);
     }
 
+    {
+        std::cout << "Testing Crear Apunte";
+       
+       // assert_equal(actual, esperado);
+    }
+
+    {
+        std::cout << "Testing Agregar Apunte a Clase";
+       
+       // assert_equal(actual, esperado);
+    }
+    
+    {
+        std::cout << "Testing Agregar Clase a Materia";
+       
+       // assert_equal(actual, esperado);
+    }
+
+    {
+        std::cout << "Testing Agregar Materia a Semestre";
+       
+       // assert_equal(actual, esperado);
+    }
+
+    {
+        std::cout << "Testing Agregar Semestre a Usuario";
+       
+       // assert_equal(actual, esperado);
+    }
+
+    {
+        std::cout << "Testing Crear Comentario";
+       
+       // assert_equal(actual, esperado);
+    } 
+
+    {
+        std::cout << "Testing Crear Apunte anonimo";
+       
+       // assert_equal(actual, esperado);
+    } 
+
+    {
+        std::cout << "Testing Agregar Comentario a Apunte";
+       
+       // assert_equal(actual, esperado);
+    } 
+
+    {
+        std::cout << "Testing Buscar tema devuelve Lista de Apuntes";
+       
+       // assert_equal(actual, esperado);
+    }
+
+    {
+        std::cout << "Testing Buscar tema devuelve Lista de Clases";
+       
+       // assert_equal(actual, esperado);
+    }
+
+    {
+        std::cout << "Testing Seguir Apunte";
+       
+       // assert_equal(actual, esperado);
+    }
+
+    {
+        std::cout << "Testing Seguir Usuario";
+       
+       // assert_equal(actual, esperado);
+    }
+
+    {
+        std::cout << "Testing Agregar Materia a Usuario";
+       
+       // assert_equal(actual, esperado);
+    }
+
+    {
+        std::cout << "Testing Modificar Apunte";
+       
+       // assert_equal(actual, esperado);
+    }
+
+    {
+        std::cout << "Testing Eliminar Apunte";
+       
+       // assert_equal(actual, esperado);
+    }
+
 }
